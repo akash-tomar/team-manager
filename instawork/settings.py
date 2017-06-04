abcbcd3b95815c77963c6a2653bfa8efec5a9c5e
Akash Tomar <akash.tomar217@gmail.com>
2017-06-04 13:28:31 +0530

    added mysql db

--- instawork/settings.py
+++ instawork/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'instawork.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'instawork',
+        'USER' : 'root',
+        'HOST' : 'localhost', 
+        'PASSWORD' : 'akash',
+        'PORT' : 3306
     }
 }
 
